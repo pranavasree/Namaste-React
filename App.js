0e32b29b455e44eb8de78e81b6de3ce3e5403980
pranavasree <pranavareddy27@gmail.com>
2024-05-04 18:53:58 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "This is namste react"),
-    React.createElement("h2", { id: "heading" }, "asdfghjkl"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
-    React.createElement("h2", { id: "heading" }, "I am an h2 tag"),
-  ]),
-]);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React.!"
-// );
-
+//React Element ==> Object ==> after renderimg then HTML Element
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+//JSX convention where we merge JS and HTML
+//JSX is HTML-like or XML-like Syntax
+//JSX (transpiled before it reaches the JS) - PARCEL - Babel.
+// const jsxHeading = <h1 className="head">Namaste React Using JSX</h1>;
+// root.render(jsxHeading);
+//React Component - Class based components -- Old way
+//and Functional Based components -- New Way
+//Functional Component is a java script function which returns a piece of JSX
+const title = <h1 className="head">Namste React</h1>;
+//Component composition - Nested components (component inside component)
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+//Rendering component syntax root.render(<HeadingComponent/>)
+root.render(<HeadingComponent />);
